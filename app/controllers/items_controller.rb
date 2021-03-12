5d4c8bfe8bc6d176dff8399e761f996e2c3db618
HIRONOBU-NAKAMURA <hironobu9dance@gmail.com>
2021-03-12 10:55:51 +0900

    修正してみました！

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,10 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
+  # あとで、authenticate_user!を記述するため、コメントアウト
+  # before_action :move_to_index, except: [:index, :show]
   def index
   end
 
-  def move_to_index
-    redirect_to action: :index unless user_signed_in?
-  end
+  # def move_to_index
+  #   redirect_to action: :index unless user_signed_in?
+  # end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true
-  validates  :password, :password_confirmation, format: { with:  /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/i }
+  with_options presence: true do
+    validates :nickname
+    validates  :password, :password_confirmation, format: { with:  /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/i }
 
-  VALID_NAME = /\A[ぁ-んァ-ヶ一-龥々]+\z/
-  validates :last_name, presence: true, format: { with: VALID_NAME }
-  validates :first_name, presence: true,
-                         format: { with: VALID_NAME }
-  VALID_KANA_NAME = /\A[ァ-ヶー－]+\z/
-  validates :kana_last_name, presence: true,
-                             format: { with: VALID_KANA_NAME }
-  validates :kana_first_name, presence: true,
+    VALID_NAME = /\A[ぁ-んァ-ヶ一-龥々]+\z/
+    validates :last_name, format: { with: VALID_NAME }
+    validates :first_name,
+                          format: { with: VALID_NAME }
+    VALID_KANA_NAME = /\A[ァ-ヶー－]+\z/
+    validates :kana_last_name,
                               format: { with: VALID_KANA_NAME }
-  validates :birthday, presence: true
+    validates :kana_first_name,
+                                format: { with: VALID_KANA_NAME }
+    validates :birthday
+  end
 end
